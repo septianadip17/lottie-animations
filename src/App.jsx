e720356f3485225421462c5a82415fd4c8042ab3
Septian Adi Pratama <septianadip7998@gmail.com>
2024-10-31 00:01:15 +0700

    alert

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Lottie from "lottie-react";
 import Cloud from "./assets/animations/cloud.json";
 
 const App = () => {
-
   return (
     <div className=" text-white">
       <div className=" justify-center items-center grid space-y-5 mt-52">
@@ -14,7 +13,10 @@ const App = () => {
         </h1>
 
         <div className="flex justify-center">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => console.log("clicked")}>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => alert("clicked")}
+          >
             {" "}
             Click me
           </button>
