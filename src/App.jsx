26ce23577a209a9f665331a441c5f19bf8eef10b
Septian Adi Pratama <septianadip7998@gmail.com>
2024-10-30 23:58:50 +0700

    react vite

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <div className=" text-white">
       <div className=" justify-center items-center grid space-y-5 mt-52">
+        <p className=" text-center">React + Vite</p>
         <p className=" text-center">Lottie Animations</p>
         <Lottie animationData={Cloud} />
         <h1 className=" text-purple-500 text-5xl font-bold  ">
